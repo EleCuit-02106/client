233f4fc61e56791a5b27ba429ca49d1aab3a2cbb
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2022-10-24 00:09:35 +0900

    Revert "SchemaとMasterDataの導入、IdDomain,Title,CopyRight追加"

--- EleCuit/Assets/Common/AssetAddresses.cs
+++ EleCuit/Assets/Common/AssetAddresses.cs
@@ -1,25 +1,13 @@
-using System.Linq;
-using UnityEngine.AddressableAssets;
-using Cysharp.Threading.Tasks;
 
 namespace EC
 {
 
     public static class Adds
     {
-        #region constant
         public const string Assets = "Assets/";
         public const string dUI = "Assets/Internals/dUnitility/Sources/UI/dUI/";
-        public const string MasterJson = "Assets/Data/Data/Master/Json/";
         public const string Stocks = "Assets/Stocks/";
         public const string GUIProKitSprite = "Assets/Stocks/GUI PRO Kit - Casual Game/ResourcesData/Sprite/";
-        #endregion
-
-        #region public static
-        /// <summary> Addressablesのアドレス存在確認 </summary>
-        public static async UniTask<bool> Exist(string address)
-            => (await Addressables.LoadResourceLocationsAsync(address)).Any();
-        #endregion
     }
 
 }
